b508902b564d9a94772f80dbb301e1144bd60b60
ekrzeptowski <contact@ekrzeptowski.pl>
2023-09-20 11:13:54 +0200

    Add auth url api endpoint

---
+++ app/api/auth-url/route.ts
@@ -0,0 +1,49 @@
+import generator, { detector } from 'megalodon';
+import { NextResponse } from 'next/server';
+import { z } from 'zod';
+
+const urlSchema = z.string().url();
+
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  try {
+    const serverUrl = urlSchema.parse(searchParams.get('server') as string);
+    const sns = await detector(serverUrl);
+    if (!sns) {
+      return NextResponse.json({
+        success: false,
+        error: 'Server not found',
+      });
+    }
+    const client = generator(sns, serverUrl);
+    return client
+      .registerApp('FediPost', {
+        scopes: ['read', 'write'],
+        website: process.env.WEBSITE_URL || 'http://localhost:3000',
+        redirect_uris:
+          (process.env.WEBSITE_URL || 'http://localhost:3000') +
+          '/auth/callback',
+      })
+      .then((appdata) => {
+        return NextResponse.json({
+          success: true,
+          clientId: appdata.clientId,
+          clientSecret: appdata.clientSecret,
+          sns,
+          url: appdata.url,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        return NextResponse.json({
+          success: false,
+          error: 'Server not found',
+        });
+      });
+  } catch (err) {
+    return NextResponse.json({
+      success: false,
+      error: 'Invalid URL',
+    });
+  }
+}
--- next.config.js
+++ next.config.js
@@ -1,4 +1,26 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+            config.resolve.fallback = {
+                net: false,
+                tls: false,
+                dns: false,
+                zlib: false,
+                stream: require.resolve('stream-browserify'),
+                events: require.resolve('events/'),
+                buffer: require.resolve('buffer/'),
+                url: require.resolve('url/'),
+                http: require.resolve('stream-http'),
+                https: require.resolve('https-browserify'),
+                crypto: require.resolve('crypto-browserify'),
+                querystring: require.resolve('querystring-es3'),
+                os: require.resolve('os-browserify/browser'),
+                assert: require.resolve('assert/')
+            }
+        }
+        return config
+    }
+}
 
 module.exports = nextConfig

48eeb724ec839f113cd592b15d1b1e69073fd115
Ewelina Krzeptowska <kontakt@ekrzeptowski.pl>
2024-01-02 19:52:11 +0100

    Fix aria-label for date-time-picker

--- components/ui/date-time-picker/date-time-picker.tsx
+++ components/ui/date-time-picker/date-time-picker.tsx
@@ -1,21 +1,21 @@
-"use client";
+'use client';
 
-import { CalendarIcon } from "lucide-react";
-import React, { useRef, useState } from "react";
+import { CalendarIcon } from 'lucide-react';
+import React, { useRef, useState } from 'react';
 import {
   DateValue,
   useButton,
   useDatePicker,
   useInteractOutside,
-} from "react-aria";
-import { DatePickerStateOptions, useDatePickerState } from "react-stately";
-import { useForwardedRef } from "@/lib/useForwardedRef";
-import { cn } from "@/lib/utils";
-import { Button } from "../button";
-import { Popover, PopoverContent, PopoverTrigger } from "../popover";
-import { Calendar } from "./calendar";
-import { DateField } from "./date-field";
-import { TimeField } from "./time-field";
+} from 'react-aria';
+import { DatePickerStateOptions, useDatePickerState } from 'react-stately';
+import { useForwardedRef } from '@/lib/useForwardedRef';
+import { cn } from '@/lib/utils';
+import { Button } from '../button';
+import { Popover, PopoverContent, PopoverTrigger } from '../popover';
+import { Calendar } from './calendar';
+import { DateField } from './date-field';
+import { TimeField } from './time-field';
 
 const DateTimePicker = React.forwardRef<
   HTMLDivElement,
@@ -49,29 +49,33 @@ const DateTimePicker = React.forwardRef<
       ref={ref}
       className={cn(
         groupProps.className,
-        "flex items-center rounded-md ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2"
+        'flex items-center rounded-md ring-offset-background focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2'
       )}
     >
-      <DateField {...fieldProps} />
+      <DateField
+        {...fieldProps}
+        aria-label={'Date ' + (state.hasTime ? 'and time ' : '') + 'input'}
+      />
       <Popover open={open} onOpenChange={setOpen}>
         <PopoverTrigger asChild>
           <Button
             {...buttonProps}
-            variant="outline"
-            className="rounded-l-none"
+            variant='outline'
+            className='rounded-l-none'
             disabled={props.isDisabled}
             onClick={() => setOpen(true)}
           >
-            <CalendarIcon className="h-5 w-5" />
+            <CalendarIcon className='h-5 w-5' />
           </Button>
         </PopoverTrigger>
-        <PopoverContent ref={contentRef} className="w-full">
-          <div {...dialogProps} className="space-y-3">
+        <PopoverContent ref={contentRef} className='w-full'>
+          <div {...dialogProps} className='space-y-3'>
             <Calendar {...calendarProps} />
             {!!state.hasTime && (
               <TimeField
                 value={state.timeValue}
                 onChange={state.setTimeValue}
+                aria-label='Time input'
                 hideTimeZone
               />
             )}
@@ -82,6 +86,6 @@ const DateTimePicker = React.forwardRef<
   );
 });
 
-DateTimePicker.displayName = "DateTimePicker";
+DateTimePicker.displayName = 'DateTimePicker';
 
 export { DateTimePicker };
